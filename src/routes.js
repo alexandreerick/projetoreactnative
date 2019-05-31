02c6caa43b2d59e459dcab9385a64e50eeb799c1
fuinho <erickalexandrex@gmail.com>
2019-05-31 20:35:32 -0300

    integrando webview e finalizando

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { createStackNavigator } from 'react-navigation';
 
 import Main from './pages/main';
+import Product from './pages/product';
 
-export default createStackNavigator({
-  Main
+export default createStackNavigator(
+{
+  Main,
+  Product
 }, {
   navigationOptions: {
     headerStyle: {
